feeeb6e14dd5e40a6a985a272e3ff07ab7bf518e
medzz1osm <mediz.osm@gmail.com>
2024-03-06 14:52:33 +0100

    requierments file moved

--- tvergaglig/multi sol/main.py
+++ tvergaglig/multi sol/main.py
@@ -6,7 +6,7 @@ import arcade
 # Screen title and size
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
-SCREEN_TITLE = "Drag and Drop Cards"
+SCREEN_TITLE = "Multi-Solitare"
 
 
 class MyGame(arcade.Window):
