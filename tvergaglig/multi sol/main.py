86a9fa04e45e8ddf4844fd010d941aef03aed02d
medzz1osm <mediz.osm@gmail.com>
2024-03-11 09:59:25 +0100

    cards

--- tvergaglig/multi sol/main.py
+++ tvergaglig/multi sol/main.py
@@ -7,7 +7,7 @@ SCREEN_TITLE = "Multi solitare"
 class MultiSolitare(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.GREEN)
+        arcade.set_background_color(arcade.color.GO_GREEN)
 
     def on_draw(self):
         arcade.start_render()
@@ -27,8 +27,29 @@ card_images = {}
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 
-for suit in suits:
-    for rank in ranks:
-        filename = f"Downloads/{2}_of_{Hearts}.ai"  
-        card_images[(suit, rank)] = arcade.load_texture(heart_2.ai)
+
+class MyGame(arcade.Window):
+    """ Main application class. """
+
+    def __init__(self):
+        super().__init__(800, 600, "Display Card Example")
+      
+
+        # Load card images
+        self.card_image = arcade.load_texture("2_of_Diamonds.png")
+
+    def on_draw(self):
+        """ Render the screen. """
+        arcade.start_render()
+
+        # Display the card image
+        arcade.draw_texture_rectangle(400, 300, self.card_image.width, self.card_image.height, self.card_image)
+
+def main():
+    """ Main function """
+    window = MyGame()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
 
