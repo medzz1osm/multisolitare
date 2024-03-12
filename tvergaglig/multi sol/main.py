957204272a7d00bc16bf4adaba1b879e17e47777
medzz1osm <mediz.osm@gmail.com>
2024-03-12 11:00:34 +0100

    added the rest of the card deck and backside

--- tvergaglig/multi sol/main.py
+++ tvergaglig/multi sol/main.py
@@ -1,55 +1,36 @@
 import arcade
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Multi solitare"
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Multi solitaire"
 
-class MultiSolitare(arcade.Window):
+class MultiSolitaire(arcade.Window):
     def __init__(self, width, height, title):
         super().__init__(width, height, title)
-        arcade.set_background_color(arcade.color.GREEN)
-
-    def on_draw(self):
-        arcade.start_render()
-
-def main():
-    game = MultiSolitare(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-    arcade.run()
-
-if __name__ == "__main__":
-    main()
-
-
-# cards 
-
-# Load card images
-card_images = {}
-suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-
-
-class MyGame(arcade.Window):
-    """ Main application class. """
-
-    def __init__(self):
-        super().__init__(800, 600, "Display Card Example")
-        arcade.set_background_color(arcade.color.WHITE)
+        arcade.set_background_color(arcade.color.GO_GREEN)
 
         # Load card images
-        self.card_image = arcade.load_texture("2_of_Diamonds.png")
+        self.card_images = {}
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+
+        for suit in suits:
+            for rank in ranks:
+                filename = f"cards/yuio.png"  # Corrected line
+                self.card_images[('Diamonds', 'Diamonds')] = arcade.load_texture("cards/2_of_Diamonds.png")
 
     def on_draw(self):
-        """ Render the screen. """
         arcade.start_render()
 
-        # Display the card image
-        arcade.draw_texture_rectangle(400, 300, self.card_image.width, self.card_image.height, self.card_image)
+        # Draw a card example
+        suit = 'Diamonds'  # Example suit
+        rank = '2'       # Example rank
+        card_texture = self.card_images[('Diamonds', 'Diamonds')]
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, card_texture.width, card_texture.height, card_texture)
 
 def main():
-    """ Main function """
-    window = MyGame()
+    game = MultiSolitaire(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
 
 if __name__ == "__main__":
     main()
-
