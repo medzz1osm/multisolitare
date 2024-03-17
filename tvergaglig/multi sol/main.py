43eb7cad0ae8872bf2b2d826f03916d64c45e66c
medzz1osm <mediz.osm@gmail.com>
2024-03-17 20:45:24 +0100

    test på login side

--- tvergaglig/multi sol/main.py
+++ tvergaglig/multi sol/main.py
@@ -1,68 +1,34 @@
-from flask import Flask
+from flask import Flask, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
 
-@app.route('/')
-def home():
-    return "Hello from Flask"
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
 
-if __name__== '__main__':
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        # Here you would add the user to the database
+        return 'Thanks for registering!'
+    elif request.method == 'GET':
+        return '''
+            <form method="POST">
+                Username: <input type="text" name="username"><br>
+                Email: <input type="text" name="email"><br>
+                Password: <input type="password" name="password"><br>
+                <input type="submit" value="Register">
+            </form>
+        '''
+    else:
+        return 'Form not submitted'
+
+if __name__ == '__main__':
     app.run(debug=True)
-
-import arcade
-
-# Screen title and size
-SCREEN_WIDTH = 1024
-SCREEN_HEIGHT = 768
-SCREEN_TITLE = "Drag and Drop Cards"
-
-
-class MyGame(arcade.Window):
-    """ Main application class. """
-
-    def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
-        arcade.set_background_color(arcade.color.AMAZON)
-
-    def setup(self):
-        """ Set up the game here. Call this function to restart the game. """
-        pass
-
-    def on_draw(self):
-        """ Render the screen. """
-        # Clear the screen
-        self.clear()
-
-        # Draw a rectangle
-        arcade.draw_rectangle_filled(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                                     200, 100, arcade.color.RED)
-
-        # Draw text
-        arcade.draw_text("Hello, Arcade!", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
-                         arcade.color.WHITE, font_size=24, anchor_x="center")
-
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        """ Called when the user presses a mouse button. """
-        pass
-
-    def on_mouse_release(self, x: float, y: float, button: int,
-                         modifiers: int):
-        """ Called when the user presses a mouse button. """
-        pass
-
-    def on_mouse_motion(self, x: float, y: float, dx: float, dy: float):
-        """ User moves mouse """
-        pass
-
-
-def main():
-    """ Main function """
-    window = MyGame()
-    window.setup()
-    arcade.run()
-
-
-if __name__ == "__main__":
-    main()
-
